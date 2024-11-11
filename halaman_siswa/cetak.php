699e8f2516cf3536965b94fb1d2332daecc2d3ea
Taufik-first <opikpik481gmail.com>
2024-11-11 17:03:21 +0800

    perbaruan halaman siswa

---
+++ halaman_siswa/cetak.php
@@ -0,0 +1,22 @@
+<?php 
+require_once 'vendor/dompdf/autoload.inc.php';
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+$html = file_get_contents('format.php');
+
+$dompdf->loadHtml($html);
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream("contoh_pdf",["Attachment" => 0]);
+
+
+?>
